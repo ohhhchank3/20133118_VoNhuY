0b6a6b3aca0ce43dac93d340d5a6a17240b38126
ohhhchank3 <94964096+ohhhchank3@users.noreply.github.com>
2023-12-16 18:18:42 +0700

    Update bard_api.py

--- bard_api.py
+++ bard_api.py
@@ -22,11 +22,11 @@ session = requests.Session()
 session.headers = SESSION_HEADERS
 
 session.cookies.set("__Secure-1PSID","eAjOKiNPwlQLDpSFLWY8fpBHMd7hEVobhROPX7mRp6ZISrljhCT0_oOR8mB-kGVgm2waXg.")
-session.cookies.set("__Secure-1PSIDTS", "sidts-CjEBPVxjSomDCWRWS5QqbjgMVZFSEuSSlA2nyg3G6iHqBL_CIGFvspCmDFL7WpdcFZuqEAA")
-session.cookies.set("__Secure-1PSIDCC", "ACA-OxO-YUwxJC9dyCR4YwMw4ckHRezMGeDVNP5rUUJLRpbyKmOkOYN6bRwl-dMU0rcbo2tuE4B_")
+session.cookies.set("__Secure-1PSIDTS", "sidts-CjEBPVxjSgSYvX_091bJQCoN5Q20Xs9Mjx1LZxewOhGWRKhIP9oaElq93hgGSAXf-w0DEAA")
+session.cookies.set("__Secure-1PSIDCC", "ABTWhQG3k3nefXqouOlzCl42LDJE_2wVsa1m9z-yHN4Ax4jbpPQkMwNT3xy91CeufBZg-Jy1j4d4")
 
-#bard = Bard(token="eAjOKiNPwlQLDpSFLWY8fpBHMd7hEVobhROPX7mRp6ZISrljhCT0_oOR8mB-kGVgm2waXg.", session=session)
-bard = BardCookies(token_from_browser=True,conversation_id='c_7a7d053b12de17bb')
+bard = Bard(token="eAjOKiNPwlQLDpSFLWY8fpBHMd7hEVobhROPX7mRp6ZISrljhCT0_oOR8mB-kGVgm2waXg.", session=session)
+#bard = BardCookies(token_from_browser=True,conversation_id='c_7a7d053b12de17bb')
 
 def send_message(message):
     try:
