c2a8d99e80591ec5b8286bb01019f200db204c12
ohhhchank3 <94964096+ohhhchank3@users.noreply.github.com>
2023-12-16 18:41:12 +0700

    Update bard_api.py

--- bard_api.py
+++ bard_api.py
@@ -4,19 +4,7 @@ from bardapi import Bard
 from bardapi.constants import SESSION_HEADERS
 
 def initialize_bard_session(_1PSID, _1PSIDTS, _1PSIDCC):
-    session = requests.Session()
-    session.headers = SESSION_HEADERS
-    session.cookies.set("__Secure-1PSID", "dQg50xvxqQlaPFv-HIPCMdHRatpM2rvBJsUtI6qK-x1MCrG20brE4QEXp9e61BEogRimVw.")
-    session.cookies.set("__Secure-1PSIDTS", _1PSIDTS)
-    session.cookies.set("__Secure-1PSIDCC","ABTWhQFWXTZGjlvHBw4FjCE-LVadjOUbeDK0N3E6Wzvj8OJ4LxPi-Ukt6jn8MGyomTgTv0M6Mpg")
-    session.headers = {
-        "Host": "bard.google.com",
-        "X-Same-Domain": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.4472.114 Safari/537.36",
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "Origin": "https://bard.google.com",
-        "Referer": "https://bard.google.com/",
-    }
+
 from bardapi import Bard, BardCookies
 session = requests.Session()
 session.headers = SESSION_HEADERS
@@ -26,7 +14,6 @@ session.cookies.set("__Secure-1PSIDTS", "sidts-CjEBPVxjSgSYvX_091bJQCoN5Q20Xs9Mj
 session.cookies.set("__Secure-1PSIDCC", "ABTWhQG3k3nefXqouOlzCl42LDJE_2wVsa1m9z-yHN4Ax4jbpPQkMwNT3xy91CeufBZg-Jy1j4d4")
 
 bard = Bard(token="eAjOKiNPwlQLDpSFLWY8fpBHMd7hEVobhROPX7mRp6ZISrljhCT0_oOR8mB-kGVgm2waXg.", session=session)
-#bard = BardCookies(token_from_browser=True,conversation_id='c_7a7d053b12de17bb')
 
 def send_message(message):
     try:
