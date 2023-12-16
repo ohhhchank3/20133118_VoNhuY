312586881214c84a1e33d569376d064459fb10bc
ohhhchank3 <94964096+ohhhchank3@users.noreply.github.com>
2023-12-16 13:03:57 +0700

    Update openAI_API_using_APIkey.py

--- openAI_API_using_APIkey.py
+++ openAI_API_using_APIkey.py
@@ -1,17 +1,34 @@
-import os
-import openai
+import requests
 
+def main(messages, model, apikey):
+    url = "https://api.openai.com/v1/chat/completions"
+    api_key = "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"
 
-def main(messages, model,apikey):
-    openai.api_key = apikey
-
-    completion = openai.ChatCompletion.create(
-        model= model,
-        messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    data = {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": "You are an assistant."},
             {"role": "user", "content": messages}
         ]
-    )
+    }
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Nếu có lỗi trong response, nó sẽ ném ra một ngoại lệ
+        result_json = response.json()
+        # Lấy nội dung từ đối tượng JSON
+        content = result_json['choices'][0]['message']['content']
+        return content
+    except requests.exceptions.RequestException as e:
+        content = f"Request failed: {e}"
+        return content
+    except Exception as e:
+        # Xử lý các ngoại lệ khác
+        content = f"An unexpected error occurred: {e}"
+        return content
 
-    return completion['choices'][0]['message']['content']
+
 
